fafbcbbd0321d36d91584cd7efec1eeb1aa91776
Avik Pal <avikpal@iitk.ac.in>
2019-03-25 07:32:55 +0530

    Add camera to handle primary rays in a clean manner

--- src/RayTracer.jl
+++ src/RayTracer.jl
@@ -2,11 +2,14 @@ module RayTracer
 
 export raytrace, intersect
 export Vec3, rgb, norm
-export SimpleSphere, CheckeredSphere, SimpleCylinder, CheckeredCylinder
+export SimpleSphere, CheckeredSphere, SimpleCylinder, CheckeredCylinder,
+       Triangle
+export get_primary_rays
 
 include("utils.jl")
 include("materials.jl")
 include("objects.jl")
 include("tracer.jl")
+include("camera.jl")
 
 end
