d0daf021f0201ed2cf03e3589a6391738c989efe
Avik Pal <avikpal@iitk.ac.in>
2019-05-07 20:07:48 +0530

    Write a custom Adam implementation for now

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,11 +1,16 @@
-import Flux.Optimise.apply!
+# import Flux.Optimise.apply!
 
 # ---------- #
-# Optimizers #
+# Optimisers #
 # ---------- #
 
 """
     update!(opt, x, Δ)
+
+NOTE: This API is currently broken. I shall fix it once the Flux Optimisers
+are more Zygote friendly. Using this will allow you to use optimisers
+that are defined in this package. These optimisers are not designed to
+be efficient but are there as a proof of concept.
 
 Provides an interface to use all of the Optimizers provided by Flux.
 The type of `x` can be anything as long as the operations defined by
@@ -32,5 +37,35 @@ update!(opt, x::T, Δ::T) where {T<:Real} = x .- (apply!(opt, [x], [Δ]))[1]
 function update!(opt, x::T, Δ::T) where {T}
     map(i -> setproperty!(x, i, update!(opt, getfield(x, i), getfield(Δ, i))), fieldnames(T))
     return x
+end
+
+
+# -------- #
+# - Adam - #
+# -------- #
+
+export Adam
+
+const ϵ = 1e-8
+
+mutable struct Adam
+    η::Float64
+    β::Tuple{Float64, Float64}
+    state::IdDict
+end
+
+Adam(η = 0.001, β = (0.9, 0.999)) = Adam(η, β, IdDict())
+
+function apply!(o::Adam, x, Δ)
+    η, β = o.η, o.β
+    # NOTE: Using length(x) for storing current state is a terrible thing to do.
+    #       Let's hope not a lot of things will be broken when using this with
+    #       Flux
+    mt, vt, βp = get!(o.state, length(x), (zero(x), zero(x), β))
+    @. mt = β[1] * mt + (1 - β[1]) * Δ
+    @. vt = β[2] * vt + (1 - β[2]) * Δ^2
+    @. Δ =  mt / (1 - βp[1]) / (√(vt / (1 - βp[2])) + ϵ) * η
+    o.state[length(x)] = (mt, vt, βp .* β)
+    return Δ
 end
 
